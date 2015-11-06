e18e0891086601cef09bfd3dea7e701a05590fd8
Sheenam <Sheenam@Sheenams-MacBook-Pro.local>
2015-11-06 00:11:42 -0500

    uploading gif

--- main.js
+++ main.js
@@ -3,45 +3,44 @@ var multer  = require('multer')
 var express = require('express')
 var fs      = require('fs')
 var app = express()
-// REDIS
+//REDIS
 var client = redis.createClient(6379, '127.0.0.1', {})
 
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
-
-app.use(function(req, res, next) {
+app.use(function(req, res, next) 
+{
     console.log(req.method, req.url);
-    var url_full = req.protocol + '://' + req.get('host') + req.originalUrl;
-    client.lpush("urlvisitedlist", url_full, function(err, reply) {
-        client.ltrim("urlvisitedlist", 0, 4);
-    });
+    var addUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    
+client.lpush('urlQueue', addUrl, function(err, reply){ 
+console.log(reply); 
+});
+
+client.ltrim('urlQueue',0,4, function(err, reply){ 
+console.log(reply); 
+});
 
     next(); // Passing the request to the next handler in the stack.
 });
 
-
-app.get('/', function(req, res) {
-  res.send('hello world')
+app.get('/recent', function(req, res) {
+  client.lrange('urlQueue', 0,-1, function(err, reply){ 
+console.log(reply); 
+res.send(reply);
+});
 })
 
 app.get('/set', function(req, res) {
-    client.set("setkey", "this message will self-destruct in 10 seconds")
-    client.expire("setKey", 10)
-    res.send("Your key is set!!!")
+  client.set("key", "message only for 10 seconds");
+  client.expire("key", 10);
+  res.send("Key Value is now set for 10 seconds");
 })
+
 app.get('/get', function(req, res) {
-    client.get("setkey", function(err, value) {
-        res.send(value)
-    });
+  client.get("key", function(err,value){ res.send(value)});
 })
-
-
-app.get('/recent', function(req, res) {
-    client.lrange("urlvisitedlist", 0, -1, function(err, urls) {
-        res.send(urls);
-    });
-});
 
 
  app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
@@ -50,33 +49,47 @@ app.get('/recent', function(req, res) {
 
     if( req.files.image )
     {
- 	   fs.readFile( req.files.image.path, function (err, data) {
- 	  		if (err) throw err;
- 	  		var img = new Buffer(data).toString('base64');
- 	  		console.log(img);
-			client.lpush('imagelist', img);
- 		});
- 	}
+       fs.readFile( req.files.image.path, function (err, data) {
+            if (err) throw err;
+            var img = new Buffer(data).toString('base64');
+            console.log(img);
+            client.lpush('imageUpload',img,function(err, reply){ 
+            console.log(reply); 
+    });
+        });
+ }
 
     res.status(204).end()
- }]);
+}]);
 
- app.get('/meow',function(req, res) {
-    client.lpop("imagelist", function(err, img) {
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        });
-        res.write("<h1>\n<img src='data:my_pic.jpg;base64," + img + "'/>");
+app.get('/meow', function(req, res) {
+    
+        
+        
+
+        client.lpop("imageUpload",function(err, value){ 
+            if (err) throw err;
+            console.log(value);
+            res.writeHead(200, {'content-type':'text/html'});
+        res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+value+"'/>");
         res.end();
+        res.send();
     });
-});
+    
+    
+ });
 
-// HTTP SERVER
+
+
+app.get('/', function(req, res) {
+  res.send('hello world')
+})
+
+//HTTP SERVER
  var server = app.listen(3000, function () {
 
-   var host = server.address().address
+  var host = server.address().address
    var port = server.address().port
 
    console.log('Example app listening at http://%s:%s', host, port)
  })
-
